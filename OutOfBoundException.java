858c413c04456a7f04ff1aa20b56585726f53293
JayasriJayas <146572711+JayasriJayas@users.noreply.github.com>
2024-11-26 17:45:05 +0530

    added

--- OutOfBoundException.java
+++ OutOfBoundException.java
@@ -3,4 +3,7 @@ public class OutOfBoundException extends Exception{
 	public OutOfBoundException(String message){
 		super(message);
 	}
+	public OutOfBoundException(String message, Throwable cause){
+		super(message, cause);
+	}
 }
