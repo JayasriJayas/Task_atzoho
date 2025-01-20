9889730f0a3f60e304fd7b489b8d7e90e8aef643
JayasriJayas <146572711+JayasriJayas@users.noreply.github.com>
2025-01-20 10:52:31 +0530

    Create BirdAbstract.java

---
+++ task 5/BirdAbstract.java
@@ -0,0 +1,10 @@
+package birds;
+public abstract class BirdAbstract{
+	public void fly(){
+		System.out.println("Bird fly");
+	}
+	public void speak(){
+		System.out.println("Birds speak");
+	
+	}
+}
