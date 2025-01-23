cd62f3690b1f791ecb067e17c046b36e531d8be7
JayasriJayas <146572711+JayasriJayas@users.noreply.github.com>
2025-01-23 14:33:36 +0530

    Update Util.java

--- task6/Util.java
+++ task6/Util.java
@@ -1,1 +1,16 @@
-
+package util;
+import exception.InvalidException;
+import exception.OutOfBoundException;
+public class Util{
+	public static void validate(Object obj)throws InvalidException{
+		if(obj==null){
+			throw new InvalidException("Error :Invalid Input");
+		}
+	}
+	public static void indexCheck(int value,int length)throws OutOfBoundException{
+		if(value<0||value>length){
+			throw new OutOfBoundException("Error : IndexOutOFBound ");
+		}
+	}
+	
+}
