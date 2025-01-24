dfe15e11689965ecb0492c4ee81ab0d3582103bd
JayasriJayas <146572711+JayasriJayas@users.noreply.github.com>
2025-01-24 11:37:29 +0530

    Update Util.java

--- task6/Util.java
+++ task6/Util.java
@@ -1,4 +1,5 @@
 package util;
+import java.util.Scanner;
 import exception.InvalidException;
 import exception.OutOfBoundException;
 public class Util{
@@ -12,5 +13,14 @@ public class Util{
 			throw new OutOfBoundException("Error : IndexOutOFBound ");
 		}
 	}
+	public static String[] getStrings(int count,Scanner sc) {
+       		String[] stringArray = new String[count];
+        	for (int i = 0; i < count; i++) {
+            		stringArray[i] = sc.next();
+        	}
+        	return stringArray;
+        }
+
+
 	
 }
